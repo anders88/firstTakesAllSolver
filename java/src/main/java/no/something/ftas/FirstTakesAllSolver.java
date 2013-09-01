6a42c9e0e1a1b09abbd800dd3ea466395574636e
Anders Karlsen <karlsenanders@hotmail.com>
2013-09-01 21:53:57 +0200

    Basic solver java

--- java/src/main/java/no/something/ftas/FirstTakesAllSolver.java
+++ java/src/main/java/no/something/ftas/FirstTakesAllSolver.java
@@ -1,36 +1,32 @@
 package no.something.ftas;
 
-import java.io.*;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
 
 public class FirstTakesAllSolver {
 	private static final String BASE_URL="http://localhost:8081/";
 	private static final String PLAYER_ID="1";
 
 	public static void main(String[] args) throws Exception {
-		//String questionId = "2000001";
-		//String answer = "024ff09e-0271-4a8b-aeb4-f4cdb8890413";
-		//new FirstTakesAllSolver().sendAnswer(questionId,answer);
-        //new FirstTakesAllSolver().testNew();
-        List<String> questions = new FirstTakesAllSolver().readQuestions("Echo");
-        System.out.println(questions);
-        System.out.println(questions.size());
+        FirstTakesAllSolver firstTakesAllSolver = new FirstTakesAllSolver();
+        List<String> questions = firstTakesAllSolver.readQuestions("Echo");
+        String res = firstTakesAllSolver.postAnswer(questions);
+
+        System.out.println("Got response");
+        System.out.println(res);
 
     }
 
@@ -41,6 +37,19 @@ public class FirstTakesAllSolver {
 
         return answers;
     }
+
+    private static class PlayerAnswerDto {
+        @SuppressWarnings("UnusedDeclaration")
+        private String playerId;
+        @SuppressWarnings("UnusedDeclaration")
+        private List<String> answers;
+
+        private PlayerAnswerDto(String playerId, List<String> answers) {
+            this.playerId = playerId;
+            this.answers = answers;
+        }
+    }
+
 
     private String readUrl(URL url) throws IOException {
         URLConnection conn = url.openConnection();
@@ -57,51 +66,41 @@ public class FirstTakesAllSolver {
         }
     }
 
-    private String postJson(String url,String json) {
+
+
+    private String postJson(String url,String json) throws Exception {
         HttpClient httpClient = new DefaultHttpClient();
 
         try {
-            HttpPost request = new HttpPost(BASE_URL + "game/something");
-            StringEntity params =new StringEntity("details={\"name\":\"myname\",\"age\":\"20\"} ");
+            HttpPost request = new HttpPost(url);
+            StringEntity params =new StringEntity(json);
 
             request.setEntity(params);
 
-            httpClient.execute(request);
+            HttpResponse response = httpClient.execute(request);
 
-            return null;
-            // handle response here...
-        }catch (Exception ex) {
-            // handle exception here
+            BufferedReader rd = new BufferedReader
+                    (new InputStreamReader(response.getEntity().getContent()));
+            try {
+                String line;
+                StringBuilder textResponse = new StringBuilder();
+                while ((line = rd.readLine()) != null) {
+                    textResponse.append(line);
+                }
+                return textResponse.toString();
+            } finally {
+                rd.close();
+            }
         } finally {
             httpClient.getConnectionManager().shutdown();
         }
     }
 
-	private String sendAnswer(String questionId, String answer) throws ClientProtocolException, IOException {
-		DefaultHttpClient defaultHttpClient = new DefaultHttpClient();
-		HttpPost httpPost = new HttpPost(BASE_URL + "game/"); 
-
-		List<NameValuePair> params = new ArrayList<>();
-		params.add(new BasicNameValuePair("gamerId", PLAYER_ID));
-		params.add(new BasicNameValuePair("questionId", questionId));
-		params.add(new BasicNameValuePair("answer", answer));
-		UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params, "UTF-8");
-		httpPost.setEntity(entity);
-		
-		HttpResponse result = defaultHttpClient.execute(httpPost);
-		
-		InputStream content = result.getEntity().getContent();
-		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content));
-		
-		StringBuilder res=new StringBuilder();
-		while(bufferedReader.ready()) {
-			res.append(bufferedReader.readLine());
-		}
-		
-		System.out.println(res);
-		return res.toString();
-		
-		
-	}
+    private String postAnswer(List<String> answers) throws Exception {
+        PlayerAnswerDto playerAnswerDto = new PlayerAnswerDto(PLAYER_ID, answers);
+        Gson gson = new Gson();
+        String res = postJson(BASE_URL + "game",gson.toJson(playerAnswerDto));
+        return res;
+    }
 
 }
