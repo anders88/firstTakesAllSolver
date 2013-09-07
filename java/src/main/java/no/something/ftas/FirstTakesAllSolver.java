cce523c3d0d8dd6325511f28e886f580ac73dd68
Johannes Brodwall <jbr@exilesoft.com>
2013-09-07 17:25:27 +0200

    GSON => Json. Simplify!

--- java/src/main/java/no/something/ftas/FirstTakesAllSolver.java
+++ java/src/main/java/no/something/ftas/FirstTakesAllSolver.java
@@ -1,90 +1,58 @@
 package no.something.ftas;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class FirstTakesAllSolver {
-	private static final String BASE_URL="http://localhost:8081/";
-	private static final String PLAYER_ID="1";
+	private static final String BASE_URL="http://www.anderssandbox.com:8080/fta/";
+	private static final String PLAYER_ID="0239042";
 
 	public static void main(String[] args) throws Exception {
-        FirstTakesAllSolver firstTakesAllSolver = new FirstTakesAllSolver();
-        List<String> questions = firstTakesAllSolver.readQuestions("Echo");
-        String res = firstTakesAllSolver.postAnswer(questions);
-
-        System.out.println("Got response");
+        String questionUrl = FirstTakesAllSolver.BASE_URL +
+                "game?playerid=" + FirstTakesAllSolver.PLAYER_ID +
+                "&category=Echo";
+        System.out.println("Reading URL " + questionUrl);
+        JSONArray question = new JSONArray(readUrl(new URL(questionUrl)));
+        System.out.println(question);
+        JSONObject answer = new JSONObject();
+        answer.put("playerId", PLAYER_ID);
+        answer.put("answers", question);
+        System.out.println(answer);
+        String res = httpPost(FirstTakesAllSolver.BASE_URL + "game", answer.toString());
         System.out.println(res);
-
     }
 
-    private List<String> readQuestions(String categoryid) throws  Exception {
-        String jsonQuestions =  readUrl(new URL(BASE_URL + "game?playerid=" + PLAYER_ID + "&category=" + categoryid));
-        Gson gson = new Gson();
-        List<String> answers = gson.fromJson(jsonQuestions, new TypeToken<List<String>>() {}.getType());
-
-        return answers;
+    private static String readUrl(URL url) throws IOException {
+        return toString(url.openConnection().getInputStream());
     }
 
-    private static class PlayerAnswerDto {
-        @SuppressWarnings("UnusedDeclaration")
-        private String playerId;
-        @SuppressWarnings("UnusedDeclaration")
-        private List<String> answers;
-
-        private PlayerAnswerDto(String playerId, List<String> answers) {
-            this.playerId = playerId;
-            this.answers = answers;
+    private static String httpPost(String url, String answer) throws Exception {
+        URLConnection conn = new URL(url).openConnection();
+        conn.setDoOutput(true);
+        try (PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(),"utf-8"))) {
+            printWriter.append(answer);
         }
+        return toString(conn.getInputStream());
     }
 
-
-    private String readUrl(URL url) throws IOException {
-        URLConnection conn = url.openConnection();
-        InputStream inputStream = conn.getInputStream();
-        return readFromStream(inputStream);
-    }
-
-    private String readFromStream(InputStream inputStream) throws IOException {
-        Reader reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
-        try {
+    private static String toString(InputStream inputStream) throws IOException {
+        try (Reader reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"))) {
             StringBuilder result = new StringBuilder();
             int c;
             while ((c = reader.read()) != -1) {
                 result.append((char)c);
             }
             return result.toString();
-        } finally {
-            reader.close();
         }
     }
-
-    private String postJson(String urlstr,String json) throws Exception {
-        URL url = new URL(urlstr);
-        URLConnection conn = url.openConnection();
-
-        conn.setDoOutput(true);
-        PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(),"utf-8"));
-
-        printWriter.append(json);
-        printWriter.close();
-
-        String res = readFromStream(conn.getInputStream());
-
-        return res;
-
-    }
-
-
-    private String postAnswer(List<String> answers) throws Exception {
-        PlayerAnswerDto playerAnswerDto = new PlayerAnswerDto(PLAYER_ID, answers);
-        Gson gson = new Gson();
-        String res = postJson(BASE_URL + "game",gson.toJson(playerAnswerDto));
-        return res;
-    }
-
 }
