9812a3d03296b4c9ee9bc88295a1d16ca90c1a4e
Johannes Brodwall <jbr@exilesoft.com>
2013-12-18 14:15:43 +0100

    Simplified C# solver

--- csharp/Solver.cs
+++ csharp/Solver.cs
@@ -3,111 +3,70 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 
 namespace FirstTakesAllSolver
 {
     public class Solver
     {
         // REPLACE WITH SERVER ADDRESS
-        private const String APPLICATION_URL = "http://localhost:8081/";
-
+        private const String ApplicationUrl = "http://codingquest.herokuapp.com/";
         // REPLACE WITH YOUR PLAYER ID
-        private const String PLAYER_ID = "1";
+        private const String PlayerId = "0125442";
 
-        public Solver()
+        private static readonly WebClient Client = new WebClient();
+        private static readonly JavaScriptSerializer Serializer = new JavaScriptSerializer();
+
+        private static string CreateResponse(IEnumerable<string> answers)
         {
-        }
-
-        public string ReadUrl(string url)
-        {
-            var client = new WebClient();
-            var data = client.OpenRead(url);
-            using (StreamReader sr = new StreamReader(data))
+            return Serializer.Serialize(new Dictionary<string, object>
             {
-                return sr.ReadToEnd();
-            }
-        }
-
-        public List<string> readQuestions(String category) {
-            String json = ReadUrl(APPLICATION_URL + "game?playerid=" + PLAYER_ID + "&category=" + category);
-            Console.Out.WriteLine("QESTIONS");
-            Console.Out.WriteLine(json);
-
-            List<string> questions = new List<string>();
-
-            int pos = json.IndexOf("\"");
-
-            while (pos != -1)
-            {
-                int endpos = json.IndexOf("\"", pos + 1);
-                String question = json.Substring(pos+1, endpos - pos - 1);
-                questions.Add(question);
-                pos = json.IndexOf("\"", endpos + 1);
-            }
-
-
-            return questions;
-        }
-
-        public String postjson(String url,String json)
-        {
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            httpWebRequest.ContentType = "text/json";
-            httpWebRequest.Method = "POST";
-
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
-            {
-                streamWriter.Write(json);
-                streamWriter.Flush();
-                streamWriter.Close();
-
-                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-                {
-                    var result = streamReader.ReadToEnd();
-                    return result;
-                }
-            }
-            
-        }
-
-        public String answer(List<string> answers)
-        {
-            StringBuilder json = new StringBuilder("{\"playerId\":\"");
-            json.Append(PLAYER_ID);
-            json.Append("\", \"answers\":[");
-            bool first = true;
-            foreach (string answer in answers)
-            {
-                if (!first)
-                {
-                    json.Append(",");
-                }
-                first = false;
-                json.Append("\"");
-                json.Append(answer);
-                json.Append("\"");
-            }
-
-            json.Append("]}");
-            Console.Out.WriteLine("POSTING");
-            Console.Out.WriteLine(json);
-            String response = postjson(APPLICATION_URL + "game",json.ToString());
-            return response;
+                {"playerId", PlayerId},
+                {"answers", answers.ToList()}
+            });
         }
 
 
         public static void Main()
         {
-            Solver solver = new Solver();
-            List<String> questions = solver.readQuestions("Echo");
-            String response = solver.answer(questions);
-            Console.Out.WriteLine("RESPONSE");
-            Console.Out.WriteLine(response);
-            Console.In.Read();
+            var answer = (IAnswer)new EchoAnswer();
+            const string category = "Echo";
 
+            const string questionUrl = ApplicationUrl + "game?playerid=" + PlayerId + "&category=" + category;
+            var questionString = Client.DownloadString(questionUrl);
+            Console.WriteLine("Question from server: " + questionString);
+            var questions = Serializer.Deserialize<string[]>(questionString);
+
+            var answers = answer.AnswerQuestions(questions);
+            
+            var answerString = CreateResponse(answers);
+            Console.WriteLine("Answering: " + answerString);
+            var response = Client.UploadString(ApplicationUrl + "game", answerString);
+            Console.WriteLine("Response from server: " + response);
+            
+            Console.WriteLine("Press enter");
+            Console.In.Read();
+        }
+    }
+
+    internal interface IAnswer
+    {
+        IEnumerable<string> AnswerQuestions(IEnumerable<string> questions);
+    }
+
+    public class EchoAnswer : IAnswer
+    {
+        public IEnumerable<string> AnswerQuestions(IEnumerable<string> questions)
+        {
+            return questions.Select(AnswerSingleQuestion);
+        }
+
+        private string AnswerSingleQuestion(string question)
+        {
+            return "";
         }
     }
 }
